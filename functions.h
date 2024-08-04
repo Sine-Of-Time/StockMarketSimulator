e1b9239892aa0e1dad67689146ec5061c9e79923
Kian <68297864+Sine-Of-Time@users.noreply.github.com>
2024-08-04 19:04:21 -0400

    Update functions.h

--- functions.h
+++ functions.h
@@ -7,12 +7,9 @@ using namespace std;
 
 // Declare functions 
 void ProfitLoss(float open, float close, string name);
-void wantTransact(string ans, int amount, float price);
+void wantTransact(float price, float high, float low, string ans, int amount);
 void print_data(string name, string exchange, string currency, float open, float high, float low, float close, float volume, float change, float price);
-void Sendsignal(float price, float high, float low, string ans, int amount);
-
 void printTerminationMessage();
-
 bool ContinueSim();
 
 #endif // FUNCTIONS_H 
